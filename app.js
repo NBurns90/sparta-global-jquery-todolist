e3997b48b7ed8fd796577d210afbb0e45344e24a
NBurns90 <natburns@gmail.com>
2018-02-01 16:13:20 +0000

    add items to list with style

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
 
   // Code goes from here
 
+  var getInput ='';
+
+  $('#submit').click(function(){
+    getInput = ($('#newTodo:text').val());
+    $('#newTodo:text').val('');
+    $('#toDoList').append($('<li class="list-item">' + getInput + '</li>'));
+    console.log(getInput);
+  });
+
+
+// append('<li class="list-item">' + getInput + '</li>');
+
+
 });
